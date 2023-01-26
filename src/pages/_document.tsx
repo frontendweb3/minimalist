3c6cac36ee096a9965c6ffa4c972c397e3aa81a7
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-26 18:19:44 +0530

    create the document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="">
       <Head />
-      <body>
+      <body style={{backgroundColor:"#eee"}}>
         <Main />
         <NextScript />
       </body>
