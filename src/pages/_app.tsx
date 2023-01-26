7467ec721df7e5c3df60ab2ed64a05658704556c
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-26 18:19:20 +0530

    create the layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import  "@/styles/global.css";
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  
+  return <>
+  <Header />
+  <Component {...pageProps} />
+  <Footer />
+  </> 
+
 }
