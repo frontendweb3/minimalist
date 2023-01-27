2287a4534d25aa21d95a0ee51bf54d108c236c66
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-28 00:02:52 +0530

    Adding the flowbite javascript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,20 @@ import type { AppProps } from 'next/app'
 import  "@/styles/global.css";
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-
+import Script from 'next/script'
 export default function App({ Component, pageProps }: AppProps) {
 
   
   return <>
-  <Header />
-  <Component {...pageProps} />
-  <Footer />
-  </> 
+  
+            <Header />
+
+            <Component {...pageProps} />
+
+            <Footer />
+
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></Script>
+
+          </> 
 
 }
