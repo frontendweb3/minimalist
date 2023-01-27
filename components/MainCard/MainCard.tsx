fba8af99f93ead42fbecec87aa6120bf46b928e7
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-28 00:01:33 +0530

    Remove the anchor tag and convert into div

--- components/MainCard/MainCard.tsx
+++ components/MainCard/MainCard.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 export default function Card() {
   return (
     <>
-      <a href="/page/read/my-now-is" className="my-16 p-4 sm:6 md:p-12 lg:p-12 mx-auto flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row md:max-w-full justify-center hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <div className="my-16 p-4 sm:6 md:p-12 lg:p-12 mx-auto flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row md:max-w-full justify-center hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
 
       <img className="h-full w-full lg:h-[350px] lg:w-[475px] lg:order-2 object-cover rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://cdn.pixabay.com/photo/2020/12/13/16/37/woman-5828787_960_720.jpg" alt=" Learn is here " />
       
@@ -16,7 +16,7 @@ export default function Card() {
         <Link href="/read/my-now-is" className="text-center w-44 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none  font-medium rounded-sm text-sm px-5 py-2.5 mr-2 my-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Read Now</Link>
       </div>
         
-      </a>
+      </div>
 
     </>
   )
