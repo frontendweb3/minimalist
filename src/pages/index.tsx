5970ebee1c4604530b70b7d74d461774eba9b750
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-29 19:48:21 +0530

    remove the unused code and adding dynami code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,25 @@ import React from 'react';
 import { NextSeo } from 'next-seo';
 import MainCard from '@/components/MainCard/MainCard';
 import Card from '@/components/Card/Card';
-
-import { Inter } from '@next/font/google'
-
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Pagination from "@/components/Pagination/Pagination"
+import _ from 'lodash';
+import { allPosts } from 'contentlayer/generated';
+import { paginate } from '../../utility/utility.ts';
 
 
-export default function Index() {
+export default function Index({post,pagination}) {
 
 
   return (
     <>
       <NextSeo
-        title={"generalSettings.title"}
-        description={"generalSettings.description"}
+        title={"Minimalist -Template is a blog"}
+        description={"The Minimalist template is a blog template built with nextjs and tailwind CSS. It provides a minimalist look for the reader and is made with modern technology, so you can easily extend and edit it."}
         canonical="https://www.rajdeepsingh.dev/"
         openGraph={{
           url: 'https://www.rajdeepsingh.dev/',
-          title: "generalSettings.title",
-          description: " generalSettings.description",
+          title: "Minimalist -Template is a blog",
+          description: "The Minimalist template is a blog template built with nextjs and tailwind CSS. It provides a minimalist look for the reader and is made with modern technology, so you can easily extend and edit it.",
           images: [{
             url: "logo",
             alt: "Rajdeep Singh --Learn With Me",
@@ -37,26 +35,42 @@ export default function Index() {
         }} />
 
 
-      <MainCard />
+      <MainCard item={post[0]} />
 
-<h2 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white px-10">Article -</h2>
+      <h2 className="container text-4xl font-bold tracking-tight text-gray-900 dark:text-white my-10">All Article -</h2>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 md:gap-5 justify-items-center mx-auto'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-5 justify-items-center mx-auto'>
+        
+        {
+          post.map(
+            (item)=> <Card item={item} key={item.title} />
+          )
+        }
+
+        
       </div>
 
-<div className='flex flex-row mx-auto justify-center w-full my-8'>
-  <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">1</button>
-  <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">2</button>
-  <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">3</button>
-</div>
-
+      <Pagination pagination={pagination} />
     </>
   )
 }
 
+
+
+export async function getStaticProps() {
+
+  let page_par_posts= process.env.PAGE_PAR_POSTS;
+
+  const sigle= _.slice(allPosts, 0, 1)
+  const posts =  _.slice(allPosts, 1, page_par_posts)
+
+  let pagination = {
+    page_par_posts:page_par_posts,
+    pageCount: paginate()
+  }
+  
+
+  return {
+    props: { sigle:sigle, post: posts,pagination },
+  }
+}
