1773cbbb104b838e629975b537c55530f4f868ab
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-29 19:34:56 +0530

    Adding the next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const { withContentlayer } = require('next-contentlayer')
+
 const nextConfig = {
   reactStrictMode: true,
 }
-
-module.exports = nextConfig
+ 
+module.exports = withContentlayer(nextConfig)
