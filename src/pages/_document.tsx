26eb2a656177cf7a06a5875b5d6c78316631de55
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-30 22:13:58 +0530

    change the background color website

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className="dark">
+    <Html lang="en" className="light">
       <Head />
       <body className='bg-gray-100 dark:bg-gray-600'>
         <Main />
