199eb75d01c0db29e39c34bd5d8e8ffbeb7f68b7
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-30 22:13:03 +0530

    change the background color

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -15,7 +15,7 @@ function Pagination({pagination}) {
                     (item:string) =>{
 
                     return <Link key={item} href={`${ item === "1"? "/": `/pagination/${item}`}`}
-                      className={`${item === number? "text-gray-900 bg-gray-700 ": "text-gray-900 hover:bg-gray-100 focus:z-10 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 " } py-2.5 px-5 mr-2 mb-2 text-sm font-medium`}>
+                      className={`${item === number? "text-gray-500 bg-gray-700  ": "text-white bg-gray-800 hover:bg-gray-900  focus:z-10 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 " } py-2.5 px-5 mr-2 mb-2 text-sm font-medium`}>
                         {item}
                     </Link>
 
