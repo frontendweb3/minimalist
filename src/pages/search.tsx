4d352dadcda0145e1cf1638220a140ed91ff9946
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-30 22:16:21 +0530

    Add functinalty for serach

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,34 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@/components/Card/Card';
-import Link from 'next/link';
+
+import { allPosts } from 'contentlayer/generated';
 
 function search() {
+
+    const [form, setForm] = useState({
+        query: '',
+    });
+
+    const [queryPost, setQueryPost] = useState({
+        posts: [],
+    });
+
+
+    function changeText(event: { target: { value: any; }; }) {
+        setForm({
+            query: event.target.value.trim().toLowerCase()
+        })
+    }
+
+
+
+    function submitFunction() {
+
+
+        let total = allPosts.filter((item) => item.title.trim().toLowerCase().includes(form.query) === true)
+
+        setQueryPost({
+            posts : total
+        })
+
+
+    }
+
     return (
-        <div className='container mx-auto my-12'>
+        <div className='container mx-auto my-16'>
 
-            <form className='my-10'>
-                
-                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                
-                <div className="relative">
 
-                    <input type="search" id="default-search" className="block w-full py-4 
+
+            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+
+            <div className="relative">
+
+                <input value={form.query} onChange={changeText} type="search" id="default-search" className="block w-full py-4 
                     pl-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Search the blog... " required={true} />
 
-            
-                    <Link href="/read/my-now-is" className="text-white absolute right-2.5 bottom-2.5 bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Search Now</Link>
 
-                    {/* className="text-center w-44 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none  font-medium rounded-sm text-sm px-5 py-2.5 mr-2 my-3 dark:bg-gray-800 dark:focus:ring-gray-700 dark:border-gray-700" */}
+                <button onClick={submitFunction} className="text-white absolute right-2.5 bottom-2.5 bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Search Now</button>
 
-                </div>
-            </form>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-5 justify-items-center mx-auto'>
-                <Card />
-                <Card />
+            </div>
+
+
+            <div className='my-16 grid grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-5 justify-items-center mx-auto'>
+
+
+                {
+
+                    queryPost.posts? queryPost.posts?.map(
+                        item=>  <Card item={item} />
+                    ): ""
+                }
             </div>
         </div>
-    );
+    )
 }
 
 export default search;
