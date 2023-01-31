c81ded6080059d6f14e6c3d256e5b8eab0243f3a
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-31 16:31:47 +0530

    Adding schema for author

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -2,7 +2,7 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/*.md`,
+  filePathPattern: `posts/**/*.md`,
   fields: {
     title: {
       type: 'string',
@@ -55,9 +55,68 @@ export const Post = defineDocumentType(() => ({
       required: true,
     },
   },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (article) => `/posts/${article._raw.flattenedPath}`,
+    },
+  }
 }))
 
+// export const Author = defineDocumentType(() => ({
+//   name: 'Author',
+//   filePathPattern: `**/*.md`,
+//   fields: {
+//     name: {
+//       type: 'string',
+//       description: 'The title of the post',
+//       required: true,
+//     },
+//     description: {
+//       type: 'string',
+//       description: 'The description of the post',
+//     },
+//     date:{
+//       type:'date',
+//       description: 'the author account create date ',
+//       required:true
+//     },
+//     draft: {
+//       type: 'boolean',
+//       description: 'The draft of the post',
+//       required: true,
+//       default: true
+//     },
+//     tags: {
+//       type: 'list',
+//       of: { type: 'string' },
+//       description: 'The list of the post',
+//     },
+//     image: {
+//       type: 'string',
+//       description: 'The image of the post',
+//       required: true,
+//     },
+//     social: {
+//       type: 'list',
+//       description: 'The list of the social',
+//     },
+//     slug: {
+//       type: 'string',
+//       description: 'The date of the post',
+//       required: true,
+//     },
+//   },
+//   computedFields: {
+//     url: {
+//       type: 'string',
+//       resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+//     },
+//   },
+// }))
+
 export default makeSource({
-  contentDirPath: 'posts',
+  contentDirPath: '.',
+  contentDirInclude:['posts'],
   documentTypes: [Post],
 })
