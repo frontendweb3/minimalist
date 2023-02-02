4d24cdfa4a2ec257fd8c3e2f6bfe9e4145f01526
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-02 23:58:51 +0530

    remove the Script tag move to _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,8 @@ import type { AppProps } from 'next/app'
 import  "@/styles/global.css";
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-import Script from 'next/script'
 export default function App({ Component, pageProps }: AppProps) {
 
-  
   return (<>
             <Header />
 
@@ -14,52 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
             <Footer />
 
-            <Script src="/flowbite.min.js"></Script>
-
-            <Script id='darkModeToggle' dangerouslySetInnerHTML={{
-                "__html":`var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-                var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-                
-                // Change the icons inside the button based on previous settings
-                if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                    themeToggleLightIcon.classList.remove('hidden');
-                } else {
-                    themeToggleDarkIcon.classList.remove('hidden');
-                }
-                
-                var themeToggleBtn = document.getElementById('theme-toggle');
-                
-                themeToggleBtn.addEventListener('click', function() {
-                
-                    // toggle icons inside button
-                    themeToggleDarkIcon.classList.toggle('hidden');
-                    themeToggleLightIcon.classList.toggle('hidden');
-                
-                    // if set via local storage previously
-                    if (localStorage.getItem('color-theme')) {
-                        if (localStorage.getItem('color-theme') === 'light') {
-                            document.documentElement.classList.add('dark');
-                            localStorage.setItem('color-theme', 'dark');
-                        } else {
-                            document.documentElement.classList.remove('dark');
-                            localStorage.setItem('color-theme', 'light');
-                        }
-                
-                    // if NOT set via local storage previously
-                    } else {
-                        if (document.documentElement.classList.contains('dark')) {
-                            document.documentElement.classList.remove('dark');
-                            localStorage.setItem('color-theme', 'light');
-                        } else {
-                            document.documentElement.classList.add('dark');
-                            localStorage.setItem('color-theme', 'dark');
-                        }
-                    }
-                    
-                });`
-              }}>
-
-            </Script>
+           
           </> )
 
 }
