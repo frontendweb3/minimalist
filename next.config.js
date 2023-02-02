c5f2a92b4ab0e710090c7dfe182157ff01cf9e01
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-02 23:56:10 +0530

    adding the trailingSlash

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { withContentlayer } = require('next-contentlayer')
 
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
 }
- 
+
 module.exports = withContentlayer(nextConfig)
