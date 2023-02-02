4b3ef3f44eca1939019886e06e896eb3c7e9bbfc
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-02 21:52:43 +0530

    define type for typescript

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { CardType } from '@/type';
 
-export default function Card({item}) {
+
+export default function Card({ item }: { item: CardType }) {
 
     return (
         <div className="w-full h-full bg-white dark:bg-gray-900">
@@ -10,7 +12,7 @@ export default function Card({item}) {
 
                 <div className="p-5">
 
-                    <Link href={`/read/${item.slug}`}>
+                    <Link href={`read/${item.slug}`}>
                         <h5 className="mb-2 px-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {item.title}
                         </h5>
@@ -19,7 +21,7 @@ export default function Card({item}) {
                     <div className="flex flex-row justify-between">
 
                         <span className="black-white dark:text-white flex flex-row  items-center">
-                            <svg width="48" height="48"  viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" className="bg-white dark:bg-white" clipRule="evenodd" d="M39.6594 23.05C34.5637 17.1 29.1652 14 24.0189 14C18.8727 14 13.4742 17.1 8.3784 23.05C7.87387 23.6 7.87387 24.45 8.3784 25C13.4742 30.9 18.8727 34 24.0189 34C29.1652 34 34.5637 30.9 39.6594 24.95C40.1135 24.4 40.1135 23.6 39.6594 23.05ZM24.0189 32C19.6295 32 14.7355 29.15 10.1947 24C14.7355 18.85 19.6295 16 24.0189 16C28.4084 16 33.3023 18.85 37.8431 24C33.3023 29.15 28.4084 32 24.0189 32ZM24.0189 18C20.689 18 17.9645 20.7 17.9645 24C17.9645 27.3 20.689 30 24.0189 30C27.3488 30 30.0733 27.3 30.0733 24C30.0733 20.7 27.3488 18 24.0189 18ZM24.0189 28C21.799 28 19.9827 26.2 19.9827 24C19.9827 21.8 21.799 20 24.0189 20C26.2389 20 28.0552 21.8 28.0552 24C28.0552 26.2 26.2389 28 24.0189 28Z" fill="currentColor" />
                             </svg>
                             2.1k
@@ -49,6 +51,8 @@ export default function Card({item}) {
 
                         </button>
 
+
+
                         <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                             <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
                                 <li>
@@ -58,13 +62,11 @@ export default function Card({item}) {
                                     <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</Link>
                                 </li>
                                 <li>
-                                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">BookMark</Link>
+                                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Bookmark us
+                                    </Link>
                                 </li>
-
                             </ul>
                         </div>
-
-
                     </div>
 
                 </div>
