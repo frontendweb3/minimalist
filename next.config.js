4003dd996c00f6fc1e5a6433c3190ece8a4edd31
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-03 01:06:48 +0530

    config the basepath in nextconfig

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const { withContentlayer } = require('next-contentlayer')
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  basePath: '/minimalist',
   images: {
     unoptimized : true
   }
