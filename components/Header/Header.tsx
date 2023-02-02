30bc9362161703ec9acbe48dd7b78e45d737cfe9
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-03 00:00:48 +0530

    fix the mobile button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -18,9 +18,18 @@ export default function Card() {
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Minimalist</span>
         </Link>
 
+        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-transparent  dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+            
+            <span className="sr-only">Open main menu</span>
 
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+            </svg>
+
+        </button>
 
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 
             <li>
@@ -36,9 +45,15 @@ export default function Card() {
             </li>
 
           </ul>
+
         </div>
 
+
+
         <div className="flex md:order-2">
+          
+         
+         
 
           <div className=" flex-row items-center hidden md:flex">
             <Link href={'/search'}>
@@ -64,11 +79,7 @@ export default function Card() {
 
           </div>
 
-
-          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-transparent  dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-          </button>
+        
 
         </div>
       </div>
