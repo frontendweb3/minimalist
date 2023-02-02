812088ccdb494fe30939797d341d82609be92348
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-03 00:59:42 +0530

    config the unoptimized image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const { withContentlayer } = require('next-contentlayer')
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  images: {
+    unoptimized : true
+  }
 }
 
 module.exports = withContentlayer(nextConfig)
