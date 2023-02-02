67f1221afba2375189953f1cbffe060373888166
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-02 21:59:11 +0530

    define type for typescript

--- components/ReadMorePost/ReadMorePost.tsx
+++ components/ReadMorePost/ReadMorePost.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import Card from '@/components/Card/Card';
+import { Posts, Post } from '@/type';
+function ReadMorePost({ posts }: {posts:Posts}) {
+  
+  return (
+    <div className="container my-10 mx-auto">
 
-function ReadMorePost({posts}) {
-    return (
-            <div className="container my-10 mx-auto">
+      <h1 className="mb-10 text-4xl font-bold tracking-tight text-gray-900 dark:text-white px-10"> Read more </h1>
 
-            <h1 className="mb-10 text-4xl font-bold tracking-tight text-gray-900 dark:text-white px-10"> Read more </h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 md:gap-5 justify-items-center mx-auto'>
+        {
+          posts.map((item: Posts ) => <Card key={item.id} item={item} />)
+        }
+      </div>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 md:gap-5 justify-items-center mx-auto'>
-                {
-                  posts.map(item=> <Card item={item} />)
-                }
-            </div>
-
-            </div>
-    );
+    </div>
+  );
 }
 
 export default ReadMorePost;
+
