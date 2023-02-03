e58bc880073ae618f9ca4599d58035e3cce57ee3
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-03 12:52:00 +0530

    fix the indentation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
-
-import  "@/styles/global.css";
+import "@/styles/global.css";
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 export default function App({ Component, pageProps }: AppProps) {
@@ -10,9 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
             <Component {...pageProps} />
 
-            <Footer />
-
-           
+            <Footer />           
           </> )
 
 }
