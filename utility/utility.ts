24b6d1ca85971f5c36b39fd8567552917e69be49
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-04 21:05:10 +0530

    fix the indentation

--- utility/utility.ts
+++ utility/utility.ts
@@ -1,20 +1,25 @@
 import { allPosts } from 'contentlayer/generated';
 
+
+export const PAGE_PAR_POSTS = 8
+
+
 export function paginateUtility() {
-    let page_par_posts = 8;
+    
+    let page_par_posts = PAGE_PAR_POSTS;
 
     let getPaths = [];
 
     for (let index = 0; index < Math.ceil(allPosts.length / page_par_posts) ; index++) {
         getPaths.push({  params: { number: `${index+1}` } })
     }
-
     return getPaths
+
 }
 
 export function paginate(){
     
-    let page_par_posts = 8;
+    let page_par_posts = PAGE_PAR_POSTS;
 
 
     let paths = [];
@@ -29,5 +34,3 @@ export function paginate(){
 }
 
 export const domain = process.env.NODE_ENV === 'development'? "http://localhost:3000/" :'https://frontendweb3.github.io/minimalist/'
-
-export const PAGE_PAR_POSTS = 8
