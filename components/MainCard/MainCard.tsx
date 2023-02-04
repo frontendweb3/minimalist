d5636658d2ad2aef77d6c5df643bd290f297283d
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-04 22:10:02 +0530

    Adding justify evenly in maincard

--- components/MainCard/MainCard.tsx
+++ components/MainCard/MainCard.tsx
@@ -7,14 +7,15 @@ export default function Card({item}:{item: MainType}) {
 
   return (
     <>
-      <div className="my-16 p-4 sm:6 md:p-20 lg:p-20 mx-auto flex flex-col items-center bg-white lg:flex-row md:max-w-full justify-center dark:bg-gray-900 ">
+      <div className="my-16 p-4 sm:6 md:p-20 lg:p-20 mx-auto flex flex-col items-center bg-white lg:flex-row md:max-w-full 
+      justify-evenly dark:bg-gray-900">
 
         <Image priority={true} width={475} height={570} className="h-full w-full lg:h-[370px] lg:w-[570px] lg:order-2 object-cover rounded-t-lg md:rounded-none md:rounded-l-lg" 
         src={`/minimalist${item.image}`} alt={item.title} />
 
         <div className=" flex flex-col justify-between p-4 leading-normal">
 
-          <p className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{item.tags[0]}</p>
+          <p className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white"> {item.tags[0]}</p>
 
           <h2 className="mb-3 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h2>
 
