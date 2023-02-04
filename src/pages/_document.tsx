1c04b1a22f2771a12ff839a50a8e20f46d30ca70
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-04 21:09:39 +0530

    Remove the flowbite with CDN and darkModewith  Script component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -8,14 +7,6 @@ export default function Document() {
       <body className='bg-gray-100 dark:bg-gray-600'>
         <Main />
         <NextScript />
-        
-        <Script
-          src="/minimalist/flowbite.min.js"
-        />
-
-        <Script id='darkModeToggle' src="/minimalist/darkmode.js"
-        />
-
       </body>
     </Html>
   )
