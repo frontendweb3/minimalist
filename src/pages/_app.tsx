a85846b57a03411a3908125a266e62ed5fe77336
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-18 16:10:40 +0530

    Adding next-theme package and remove the Script component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,15 @@ import type { AppProps } from 'next/app'
 import "@/styles/global.css";
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-import Script from 'next/script'
+import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <>
+    <ThemeProvider attribute="class" >
       <Header />
-
       <Component {...pageProps} />
-
       <Footer />
-
-    </>
+    </ThemeProvider>
   )
 }
