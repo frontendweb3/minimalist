9d77612e07d3094eb41668176d57ecd04da0cfeb
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-18 16:09:39 +0530

    Adding Dark, Light icon and adding support of useTheme

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaSearch } from "react-icons/fa";
+import { useTheme } from 'next-themes'
+import Dark from "../icons/Dark"
+import Light from "../icons/Light"
 
 export default function Header() {
+
+  const { theme, setTheme } = useTheme()
+
   return (
     <header className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
 
@@ -19,12 +25,12 @@ export default function Header() {
         </Link>
 
         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-transparent  dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-            
-            <span className="sr-only">Open main menu</span>
 
-            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
-            </svg>
+          <span className="sr-only">Open main menu</span>
+
+          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+          </svg>
 
         </button>
 
@@ -49,9 +55,6 @@ export default function Header() {
         </nav>
 
         <div className="flex md:order-2">
-          
-         
-         
 
           <div className=" flex-row items-center hidden md:flex">
             <Link href={'/search'}>
@@ -66,18 +69,18 @@ export default function Header() {
             <Link target="_blank" className="ml-3" href={'https://www.linkedin.com/in/officalrajdeepsingh/'}>
               <FaLinkedinIn className="dark:text-white" />
             </Link>
-            
-            <button id="theme-toggle" type="button" className="ml-3 text-gray-500 dark:text-white focus:outline-none dark:focus:ring-gray-700 rounded-lg text-sm">
 
-              <svg id="theme-toggle-dark-icon" className="hidden w-5 h-5" fill="currentColor" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
-
-              <svg id="theme-toggle-light-icon" className="hidden w-5 h-5" fill="currentColor" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path></svg>
-
+            <button
+              id="theme-toggle"
+              type="button"
+              className="ml-3 text-gray-500 dark:text-white focus:outline-none dark:focus:ring-gray-700 rounded-lg text-sm"
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              {theme === "dark" ? <Light /> : <Dark />}
             </button>
-
           </div>
 
-        
+
 
         </div>
       </div>
