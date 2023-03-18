9e361448cfb34851e3ab23ecf91130d322425c36
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-18 15:39:57 +0530

    update the utility

--- utility/utility.ts
+++ utility/utility.ts
@@ -5,32 +5,29 @@ export const PAGE_PAR_POSTS = 8
 
 
 export function paginateUtility() {
-    
-    let page_par_posts = PAGE_PAR_POSTS;
 
-    let getPaths = [];
+  let page_par_posts = PAGE_PAR_POSTS;
 
-    for (let index = 0; index < Math.ceil(allPosts.length / page_par_posts) ; index++) {
-        getPaths.push({  params: { number: `${index+1}` } })
-    }
-    return getPaths
+  let getPaths = [];
+
+  for (let index = 0; index < Math.ceil(allPosts.length / page_par_posts); index++) {
+    getPaths.push({ params: { number: `${index + 1}` } })
+  }
+  return getPaths
 
 }
 
-export function paginate(){
-    
-    let page_par_posts = PAGE_PAR_POSTS;
+export function paginate() {
 
+  let page_par_posts = PAGE_PAR_POSTS;
 
-    let paths = [];
+  let paths = [];
 
+  for (let index = 0; index < Math.ceil(allPosts.length / page_par_posts); index++) {
+    paths.push(`${index + 1}`)
+  }
 
-    for (let index = 0; index < Math.ceil(allPosts.length / page_par_posts) ; index++) {
-        paths.push(`${index+1}`)
-    }   
-
-
-    return paths
+  return paths
 }
 
-export const domain = process.env.NODE_ENV === 'development'? "http://localhost:3000/" :'https://frontendweb3.github.io/minimalist/'
+export const domain = process.env.NODE_ENV === 'development' ? "http://localhost:3000/" : 'https://frontendweb3.github.io/'
