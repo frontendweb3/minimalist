0bfc05165ad554c1c66571c7f03d76fb94fb8a2b
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-18 15:39:08 +0530

    remove the minimalist path in image component and remove comment code

--- src/pages/read/[slug].tsx
+++ src/pages/read/[slug].tsx
@@ -6,118 +6,109 @@ import _ from 'lodash';
 import dayjs from "dayjs";
 import ReadMorePost from '@/components/ReadMorePost/ReadMorePost'
 import Image from 'next/image';
-import {ReadPage} from '@/type';
+import { ReadPage } from '@/type';
 
-// author: {
-//     name: 'Jeanne Ballard',
-//     image: '/images/women-5.jpg',
-//     social: Array(2) [ 'https://www.facebook.com/officialrajdeepsingh', 'https://twitter.com/Official_R_deep' ],
-//     type: 'Author',
-//     _raw: {}
-//   },
-
-function Read({ post, authorPost, pagination }:ReadPage) {
-
-    
-
-    return (
-        <>
-            <main className="my-16 pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
-
-                <div className="flex flex-col justify-between px-4 mx-auto max-w-screen-xl">
-
-                    <Image src={`/minimalist${post?.image}`} alt={post.title} height={650} width={1224} className="rounded-t-lg bg-fixed h-[650px] w-full bg-origin-content  bg-cover bg-no-repeat p-2" />
-
-                    <article className="my-12 prose lg:prose-xl dark:prose-invert w-full mx-auto max-w-4xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+function Read({ post, authorPost, pagination }: ReadPage) {
 
 
-                        <h1 style={{ margin: "0px" }} className="prose-h1:my-0 text-3xl font-extrabold leading-tight text-gray-900 lg:text-4xl dark:text-white">{post.title}</h1>
+  return (
+    <>
+      <main className="my-16 pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
 
-                        <p style={{ margin: "0px" }} className="text-1xl font-extrabold leading-tight text-gray-500 lg:text-xl dark:text-white">{post.description}</p>
+        <div className="flex flex-col justify-between px-4 mx-auto max-w-screen-xl">
 
-                        <address className="flex items-center mb-6 not-italic">
+          <Image src={post?.image} alt={post.title} height={650} width={1224} className="rounded-t-lg bg-fixed h-[650px] w-full bg-origin-content  bg-cover bg-no-repeat p-2" />
 
-                            <div className="inline-flex items-center text-sm text-gray-900 dark:text-white">
+          <article className="my-12 prose lg:prose-xl dark:prose-invert w-full mx-auto max-w-4xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
 
-                                <Image width={'64'} height={'64'}  className="mr-4 w-16 h-16 rounded-full" src={`/minimalist${post.author.image}`} alt={post.author.name} />
 
-                                <div className="flex flex-col">
+            <h1 style={{ margin: "0px" }} className="prose-h1:my-0 text-3xl font-extrabold leading-tight text-gray-900 lg:text-4xl dark:text-white">{post.title}</h1>
 
-                                    <Link href={`/author/${post.author.name.trim().toLowerCase().replaceAll(" ","-")}`} rel="author" className="text-xl font-bold no-underline text-gray-900 dark:text-white">
-                                        {post.author.name}
-                                    </Link>
+            <p style={{ margin: "0px" }} className="text-1xl font-extrabold leading-tight text-gray-500 lg:text-xl dark:text-white">{post.description}</p>
 
-                                    <div className="flex flex-col md:flex-row ">
-                                        <p style={{ "margin": "0px" }} className="ml-3 text-base font-light text-gray-500 dark:text-gray-400">
-                                            <time dateTime="2022-02-08" title="February 8th, 2022">
-                                                {dayjs(post.date).format("MMM DD, YYYY")}
-                                            </time>
-                                        </p>
-                                    </div>
+            <address className="flex items-center mb-6 not-italic">
 
-                                </div>
+              <div className="inline-flex items-center text-sm text-gray-900 dark:text-white">
 
-                            </div>
+                <Image width={'64'} height={'64'} className="mr-4 w-16 h-16 rounded-full" src={post.author.image} alt={post.author.name} />
 
-                        </address>
+                <div className="flex flex-col">
 
-                        <div dangerouslySetInnerHTML={{ __html: post.body.html }}>
-                        </div>
+                  <Link href={`/author/${post.author.name.trim().toLowerCase().replaceAll(" ", "-")}`} rel="author" className="text-xl font-bold no-underline text-gray-900 dark:text-white">
+                    {post.author.name}
+                  </Link>
 
-                    </article>
+                  <div className="flex flex-col md:flex-row ">
+                    <p style={{ "margin": "0px" }} className="ml-3 text-base font-light text-gray-500 dark:text-gray-400">
+                      <time dateTime="2022-02-08" title="February 8th, 2022">
+                        {dayjs(post.date).format("MMM DD, YYYY")}
+                      </time>
+                    </p>
+                  </div>
 
                 </div>
 
+              </div>
 
-            </main>
+            </address>
 
-            <div className='mx-auto w-full my-12'>
-
-                <ul className='flex flex-col items-center md:flex-row justify-center md:justify-between'>
-
-
-                    <li className='bg-white flex flex-row py-4 px-5 items-center'>
-
-
-
-                        {
-                            pagination.prev !== null ? <>
-                                <BsArrowLeft className="mr-1" />
-                                Previous :
-                                <Link href={pagination.prev.slug}>
-                                    {pagination.prev.title}
-                                </Link>
-                            </> : ''
-                        }
-
-
-                    </li>
-
-                    <li className="bg-white mt-8 md:mt-0 flex flex-row py-4 px-5 items-center">
-
-                        {
-                            pagination.next !== null ? <>
-                                Next :
-                                <Link href={pagination.next.slug}>
-                                    {pagination.next.title}
-                                </Link>
-                            </> : ""
-                        }
-
-
-
-                        <BsArrowRight className="ml-1" />
-                    </li>
-
-                </ul>
+            <div dangerouslySetInnerHTML={{ __html: post.body.html }}>
             </div>
 
+          </article>
+
+        </div>
+
+
+      </main>
+
+      <div className='mx-auto w-full my-12'>
+
+        <ul className='flex flex-col items-center md:flex-row justify-center md:justify-between'>
+
+
+          <li className='bg-white flex flex-row py-4 px-5 items-center'>
+
+
+
             {
-                authorPost ? <ReadMorePost posts={authorPost} /> : ""
+              pagination.prev !== null ? <>
+                <BsArrowLeft className="mr-1" />
+                Previous :
+                <Link href={pagination.prev.slug}>
+                  {pagination.prev.title}
+                </Link>
+              </> : ''
             }
 
-        </>
-    );
+
+          </li>
+
+          <li className="bg-white mt-8 md:mt-0 flex flex-row py-4 px-5 items-center">
+
+            {
+              pagination.next !== null ? <>
+                Next :
+                <Link href={pagination.next.slug}>
+                  {pagination.next.title}
+                </Link>
+              </> : ""
+            }
+
+
+
+            <BsArrowRight className="ml-1" />
+          </li>
+
+        </ul>
+      </div>
+
+      {
+        authorPost ? <ReadMorePost posts={authorPost} /> : ""
+      }
+
+    </>
+  );
 }
 
 export default Read;
@@ -126,43 +117,43 @@ export default Read;
 export async function getStaticPaths() {
 
 
-    let paths: { params: { slug: string; }; }[] = []
+  let paths: { params: { slug: string; }; }[] = []
 
-    allPosts.map(
-        item => {            
-            paths.push({ params: { slug: item.slug } })
-        }
-    )
-    return {
-        paths: paths,
-        fallback: false,
+  allPosts.map(
+    item => {
+      paths.push({ params: { slug: item.slug } })
     }
+  )
+  return {
+    paths: paths,
+    fallback: false,
+  }
 }
 
 
-export async function getStaticProps({ params }: { params : {slug:string}}) {
+export async function getStaticProps({ params }: { params: { slug: string } }) {
 
-    const getPaginationIndex = allPosts.findIndex((element) => element.slug === params.slug);
-    
+  const getPaginationIndex = allPosts.findIndex((element) => element.slug === params.slug);
 
-    const pagination = {
-        next: allPosts[getPaginationIndex + 1] === undefined ? null : allPosts[getPaginationIndex + 1],
-        prev: getPaginationIndex === 0 ? null : allPosts[getPaginationIndex - 1]
-    }
 
-    let post = _.filter(allPosts,  (item)=> {
+  const pagination = {
+    next: allPosts[getPaginationIndex + 1] === undefined ? null : allPosts[getPaginationIndex + 1],
+    prev: getPaginationIndex === 0 ? null : allPosts[getPaginationIndex - 1]
+  }
 
-        return item.slug === params.slug
+  let post = _.filter(allPosts, (item) => {
 
-    })
+    return item.slug === params.slug
 
-    let authorPost = _.filter(allPosts,  (item)=> {
+  })
 
-        return item.author === post[0].author
+  let authorPost = _.filter(allPosts, (item) => {
 
-    })    
-    
-    return {
-        props: { post: post[0], authorPost, pagination },
-    }
+    return item.author === post[0].author
+
+  })
+
+  return {
+    props: { post: post[0], authorPost, pagination },
+  }
 }
