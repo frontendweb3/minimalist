fceab08ce94dc70af3d4c53dd7c3c4b5dcf5e8b9
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-22 17:08:43 +0530

    fix the type script issue

--- src/pages/pagination/[number].tsx
+++ src/pages/pagination/[number].tsx
@@ -5,100 +5,93 @@ import Card from '@/components/Card/Card';
 import Pagination from "@/components/Pagination/Pagination"
 import _ from 'lodash';
 import { allPosts } from 'contentlayer/generated';
-import { IndexPage, Posts } from '@/type';
-
+import type { Post } from 'contentlayer/generated';
 import { paginateUtility, paginate, PAGE_PAR_POSTS } from '../../../utility/utility';
+import { useRouter } from 'next/router';
+export default function Index({ single, posts, pagination }: { single: Post; posts: Post[]; pagination: { page_par_posts: number; pageCount: string[]; } }) {
+  const router = useRouter()
+  return (
+    <>
 
-export default function Index({ single, posts, pagination }: IndexPage) {    
+      <NextSeo
+        title={`Minimalist index page is ${router.query.number}`}
+        description={"pagination page is from minimalist template"}
+        canonical="https://www.rajdeepsingh.dev/"
+        openGraph={{
+          url: 'https://www.rajdeepsingh.dev/',
+          title: "Minimalist -Template is a blog",
+          description: "The Minimalist template is a blog template built with nextjs and tailwind CSS. It provides a minimalist look for the reader and is made with modern technology, so you can easily extend and edit it.",
+          images: [{
+            url: "logo",
+            alt: "Rajdeep Singh --Learn With Me",
+          }
+          ],
+          site_name: 'Rajdeep Singh',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
 
-    return (
-        <>
+      <MainCard item={single} />
 
-            <NextSeo
-                title={"Minimalist -Template is a blog"}
-                description={"The Minimalist template is a blog template built with nextjs and tailwind CSS. It provides a minimalist look for the reader and is made with modern technology, so you can easily extend and edit it."}
-                canonical="https://www.rajdeepsingh.dev/"
-                openGraph={{
-                    url: 'https://www.rajdeepsingh.dev/',
-                    title: "Minimalist -Template is a blog",
-                    description: "The Minimalist template is a blog template built with nextjs and tailwind CSS. It provides a minimalist look for the reader and is made with modern technology, so you can easily extend and edit it.",
-                    images: [{
-                        url: "logo",
-                        alt: "Rajdeep Singh --Learn With Me",
-                    }
-                    ],
-                    site_name: 'Rajdeep Singh',
-                }}
-                twitter={{
-                    handle: '@handle',
-                    site: '@site',
-                    cardType: 'summary_large_image',
-                }}
-            />
+      <h2 className="container text-4xl font-bold tracking-tight text-gray-900 dark:text-white my-10">All Article -</h2>
 
-            <MainCard item={single} />
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-5 justify-items-center mx-auto'>
 
-            <h2 className="container text-4xl font-bold tracking-tight text-gray-900 dark:text-white my-10">All Article -</h2>
-
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-5 justify-items-center mx-auto'>
-
-                {
-                    posts.map(
-                        (item: Posts) => {
-                            if (item.length !== 0) {
-                                return <Card item={item} key={item.id} />
-                            }
-                        }
-                    )
-                }
+        {
+          posts.map((item) => <Card item={item} key={item.id} />)
+        }
 
 
-            </div>
+      </div>
 
-            <Pagination pagination={pagination} />
+      <Pagination pagination={pagination} />
 
-        </>
-    )
+    </>
+  )
 }
 
 
 // fetch all posts 
-export async function getStaticProps({params}: { params: { number: string; }; }) {
+export async function getStaticProps({ params }: { params: { number: string; }; }) {
 
-    let totalPosts;
-    
-    if( Number(params.number) == 1 ){
-        totalPosts = _.slice(allPosts, 0, PAGE_PAR_POSTS)
-    } 
+  let totalPosts;
+
+  if (Number(params.number) == 1) {
+    totalPosts = _.slice(allPosts, 0, PAGE_PAR_POSTS)
+  }
 
 
-    if( Number(params.number) == 2 ){
-      totalPosts = _.slice(allPosts, PAGE_PAR_POSTS, PAGE_PAR_POSTS * Number(params.number))
-    }
+  if (Number(params.number) == 2) {
+    totalPosts = _.slice(allPosts, PAGE_PAR_POSTS, PAGE_PAR_POSTS * Number(params.number))
+  }
 
-    
-    if ( Number(params.number) >  2) {
-      totalPosts = _.slice(
-           allPosts, PAGE_PAR_POSTS*Number(params.number) - PAGE_PAR_POSTS,
-           PAGE_PAR_POSTS * Number(params.number)
-        )
-    }
 
-    let pagination = {
-        pageCount: paginate()
-    }
+  if (Number(params.number) > 2) {
+    totalPosts = _.slice(
+      allPosts, PAGE_PAR_POSTS * Number(params.number) - PAGE_PAR_POSTS,
+      PAGE_PAR_POSTS * Number(params.number)
+    )
+  }
 
-    return {
-        props: { single: _.first(totalPosts), posts: _.drop(totalPosts), pagination},
-    }
-    
+  let pagination = {
+    pageCount: paginate()
+  }
+
+  return {
+    props: { single: _.first(totalPosts), posts: _.drop(totalPosts), pagination },
+  }
+
 }
 export async function getStaticPaths() {
 
-     return {
-       paths: _.drop(paginateUtility()),
-       fallback: false,
-     }
-     
-   
+  return {
+    paths: _.drop(paginateUtility()),
+    fallback: false,
+  }
+
+
 }
