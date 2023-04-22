8ed9b27a8f22111500c56fe47b4bb0a48101397c
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-22 17:08:16 +0530

    fix the typescript issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,9 @@ import Pagination from "@/components/Pagination/Pagination"
 import _ from 'lodash';
 import { allPosts } from 'contentlayer/generated';
 import { paginate, PAGE_PAR_POSTS } from '../../utility/utility';
-import { IndexPage, Posts } from '@/type';
-// import { allPosts, Post } from 'contentlayer/generated'
+import type { Post } from 'contentlayer/generated'
 
-export default function Index({ single, posts, pagination }: IndexPage) {
+export default function Index({ single, posts, pagination }: { single: Post; posts: Post[]; pagination: { page_par_posts: number; pageCount: string[]; } }) {
 
 
   return (
@@ -20,13 +19,12 @@ export default function Index({ single, posts, pagination }: IndexPage) {
         canonical="https://www.rajdeepsingh.dev/"
         openGraph={{
           url: 'https://www.rajdeepsingh.dev/',
-          title: "Minimalist -Template is a blog",
+          title: "Minimalist - Template is a blog",
           description: "The Minimalist template is a blog template built with nextjs and tailwind CSS. It provides a minimalist look for the reader and is made with modern technology, so you can easily extend and edit it.",
           images: [{
             url: "logo",
             alt: "Rajdeep Singh --Learn With Me",
-          }
-          ],
+          }],
           site_name: 'Rajdeep Singh',
         }}
         twitter={{
@@ -38,20 +36,12 @@ export default function Index({ single, posts, pagination }: IndexPage) {
 
       <MainCard item={single} />
 
-      <h2 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white py-12 pl-16" >All Article -</h2>
+      <h2 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white py-12 pl-16"> All Article -</h2>
 
       <div className='grid grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-5 justify-items-center mx-auto'>
 
         {
-          posts.map(
-            (item: Posts) => {
-              if (item.length !== 0) {
-
-                return <Card item={item} key={item.title} />
-
-              }
-            }
-          )
+          posts.map((item) => <Card item={item} key={item.title} />)
         }
       </div>
       <Pagination pagination={pagination} />
@@ -64,7 +54,9 @@ export async function getStaticProps() {
 
   let page_par_posts: number = PAGE_PAR_POSTS as number
 
-  const posts = _.slice(allPosts, 0, page_par_posts)
+  const posts: Post[] = _.slice(allPosts, 0, page_par_posts)
+
+  console.log(posts)
 
   let pagination = {
     page_par_posts: page_par_posts,
