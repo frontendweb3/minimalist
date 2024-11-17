953a1e98f1819d01b5017c5ba8d41c327a7382c8
Rajdeep Singh <radewala.2@gmail.com>
2024-11-17 21:45:13 +0530

    config the the vercel tool bar as well content layer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const { withContentlayer } = require('next-contentlayer')
+const withVercelToolbar = require('@vercel/toolbar/plugins/next')();
 
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true
-}
+};
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withVercelToolbar(withContentlayer(nextConfig))
