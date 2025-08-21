ac68eff9a912c0fc9869d522cadd389d6086b9b7
Rajdeep Singh <radewala.2@gmail.com>
2025-08-21 18:49:24 +0530

    remove the verceltoolbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,28 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import { VercelToolbar } from '@vercel/toolbar/next';
 import { Header } from "@/components/Header/Header";
 import { Footer } from "@/components/Footer/Footer";
-import { ThemeProvider } from "@/components/theme/theme-provider"
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Home | Minimalist',
-  description: 'Minimalist is template for blog built with nextjs, shadcn ui and tailwind css.',
-}
+  title: "Home | Minimalist",
+  description:
+    "Minimalist is template for blog built with nextjs, shadcn ui and tailwind css.",
+};
 
-export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-
-  const shouldInjectToolbar = process.env.NODE_ENV === 'development';
-  // const banner = await showBanner(); 
+export default async function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem={true}
+        >
           <Header />
           {children}
-          {shouldInjectToolbar && <VercelToolbar />}
           <Footer />
         </ThemeProvider>
       </body>
