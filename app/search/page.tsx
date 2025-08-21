2e674ba612e0c95fbc9f78b7a505329050c979a7
Rajdeep Singh <radewala.2@gmail.com>
2025-08-21 18:48:16 +0530

    search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import { Card } from '@/components/Card/Card';
+import React, { useState } from "react";
+import { Card } from "@/components/Card/Card";
 import { searchWithFuse } from "@/components/useFuse";
-import type { Post } from 'contentlayer/generated';
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import type { Post } from "contentlayer/generated";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface TypeResult {
   item: Post;
@@ -13,21 +13,29 @@ interface TypeResult {
 }
 
 export default function Search() {
-
   const [query, setQuery] = useState("");
 
-  const allPosts: TypeResult[] = searchWithFuse(query)
+  const allPosts: TypeResult[] = searchWithFuse(query);
 
   return (
     <section className="py-32">
-      <div className="container grid w-full max-w-lg items-center gap-4">
-          <Label htmlFor="default-search">Search Your Articles</Label>
-          <Input className="rounded-sm" onChange={(event) => setQuery(event.target.value)} type="text" id="default-search" required={true} placeholder=" Type your keywork" />
+      <div className="container grid max-w-lg items-center gap-4">
+        <Label htmlFor="default-search">Search Your Articles</Label>
+        <Input
+          className="rounded-sm"
+          onChange={(event) => setQuery(event.target.value)}
+          type="text"
+          id="default-search"
+          required={true}
+          placeholder=" Type your keywork"
+        />
       </div>
 
       <div className="container mt-24 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-8">
-        {allPosts.map((post) => <Card key={post.item.id} item={post.item} />)}
+        {allPosts.map((post) => (
+          <Card key={post.item.id} item={post.item} />
+        ))}
       </div>
-    </section >
-  )
+    </section>
+  );
 }
