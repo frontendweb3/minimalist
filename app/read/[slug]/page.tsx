f79d60e991286d775dfc495b5b95dd54c82d2105
Rajdeep Singh <radewala.2@gmail.com>
2025-08-22 22:19:23 +0530

    redesign the UI

--- app/read/[slug]/page.tsx
+++ app/read/[slug]/page.tsx
@@ -1,61 +1,97 @@
-import React from 'react';
-import Link from 'next/link';
-import { allPosts } from 'contentlayer/generated';
-import _ from 'lodash';
+import React from "react";
+import Link from "next/link";
+import { allPosts } from "contentlayer/generated";
+import _ from "lodash";
 import dayjs from "dayjs";
-import Image from 'next/image';
-import { notFound } from 'next/navigation';
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import type { Metadata } from "next";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ChevronLeft, Tag } from "lucide-react";
 
-export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
+export const generateStaticParams = async () =>
+  allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const slug = (await params).slug;
 
-export async function generateMetadata( { params }: { params: Promise<{ slug: string }> } ) {
-
-  const slug = (await params).slug
-
-  const post = allPosts.find((post) => post._raw.flattenedPath === slug)
+  const post = allPosts.find((post) => post._raw.flattenedPath === slug);
 
   if (!post) {
-    notFound()
+    notFound();
   }
 
-  return { title: post.title }
-
+  return { title: post.title };
 }
-export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
-
-  const slug = (await params).slug
-  const post = allPosts.find((post) => post._raw.flattenedPath === slug)
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+  const post = allPosts.find((post) => post._raw.flattenedPath === slug);
 
   if (!post) {
-    notFound()
+    notFound();
   }
 
   return (
-    <main className="my-16 pt-8 pb-16 lg:pt-16 lg:pb-24">
+    <main className="flex flex-col">
+      <Button
+        variant={"link"}
+        size={"icon"}
+        className="container mt-12 mb-3 mx-auto justify-start"
+      >
+        <Link className="flex flex-row gap-5 items-center" href={"/"}>
+          <ChevronLeft size={28} />
+          Back
+        </Link>
+      </Button>
 
-      <div className="flex flex-col justify-between px-4 mx-auto max-w-screen-xl">
+      <div className="relative mt-10 h-[664px] w-full">
+        <Image
+          src={post?.image}
+          alt={post.title}
+          fill={true}
+          className="object-cover bg-fixed"
+        />
+      </div>
 
-        <Image src={post?.image} alt={post.title} height={650} width={1224} className="rounded-t-lg bg-fixed h-[650px] w-full bg-origin-content  bg-cover bg-no-repeat p-2" />
+      <article className="my-24 prose prose-slate dark:prose-invert w-full mx-auto max-w-4xl">
+        <h1 className="text-3xl font-extrabold mb-3 leading-20 md:text-6xl lg:text-7xl">
+          {post.title}
+        </h1>
+        <p className="lead font-medium">{post.description}</p>
+        <div className="flex items-center justify-between mt-4 mb-6 not-italic">
+          <Link
+            href={`/author/${post.author?.trim().toLowerCase().replaceAll(" ", "-")}`}
+            rel="author"
+            className="no-underline"
+          >
+            Published By {post?.author}
+          </Link>
 
-        <article className="my-12 prose prose-slate prose-headings:text-primary prose-lead:text-secondary-foreground prose-ol:text-primary prose-ul:text-primary prose-a:text-primary prose-p:text-primary dark:prose-invert xl:prose-md w-full mx-auto max-w-4xl">
-          <h1 className="text-3xl font-extrabold mb-2 lg:text-6xl text-primary">{post.title}</h1>
-          <p className="lead font-bold">{post.description}</p>
-
-          <div className="flex items-center justify-between mt-4 mb-6 not-italic">
-
-            <Link href={`/author/${post.author?.trim().toLowerCase().replaceAll(" ", "-")}`} rel="author" className="no-underline">
-              Published By {post?.author}
-            </Link>
-
-            <time dateTime={post.date} title={dayjs(post.date).format("MMM DD, YYYY")} className="text-base text-primary">
+          <div className="flex flex-row gap-5 items-center">
+            <time
+              dateTime={post.date}
+              title={dayjs(post.date).format("MMM DD, YYYY")}
+              className="text-base text-primary"
+            >
               {dayjs(post.date).format("MMM DD, YYYY")}
             </time>
+            {post.category !== undefined && (
+              <Button variant={"ghost"}>
+                <Tag /> {post?.category[0]}
+              </Button>
+            )}
           </div>
-          <div dangerouslySetInnerHTML={{ __html: post?.body?.html }} />
-        </article>
-      </div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: post?.body?.html }} />
+      </article>
     </main>
   );
 }
-
