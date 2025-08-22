3cfffc648e423577ecbecd09837cd8cd275e4132
Rajdeep Singh <radewala.2@gmail.com>
2025-08-22 22:18:11 +0530

    redesign the hero UI

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,34 +1,51 @@
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
-import { allPosts } from 'contentlayer/generated';
-import Image from 'next/image';
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { allPosts } from "contentlayer/generated";
+import Image from "next/image";
 import Link from "next/link";
-import { ArrowRight } from 'lucide-react';
-import { Card, CardDescription, CardTitle } from "@/components/ui/card"
+import { ArrowRight, Tag } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export function Hero() {
-
-  const post = allPosts[0]
+  const post = allPosts[0];
   return (
-    <Card className="container relative py-24 rounded-sm">
-      <div className="mx-auto flex max-w-7xl flex-wrap items-center">
-        <div className="w-full rounded-sm lg:w-1/2 lg:max-w-lg">
-          <Image priority={true} width={475} height={570} className="mx-auto rounded-lg object-cover object-center" src={post.image} alt={post.title} />
-        </div>
-        <div className="mb-16 mt-12 flex flex-col items-start text-left md:mb-0 lg:w-1/2 lg:grow lg:pl-6 xl:mt-0 xl:pl-24">
-
-          {
-            post?.tags !== undefined ? <Badge className="mb-8 text-xs font-bold uppercase tracking-widest  text-center"> {post?.tags[0]} </Badge> : ""
-          }
-
-          <CardTitle className="mb-8 text-4xl font-semibold leading-none tracking-tight md:text-7xl lg:text-5xl"> {post.title}  </CardTitle>
-          <CardDescription className="mb-8 text-lg leading-relaxed text-gray-500"> {post.description} </CardDescription >
-
-          <Link href={`/read/${post.slug}`}>
-            <Button variant="secondary" className="w-full sm:w-auto hover:underline">Read more   <ArrowRight className="ml-2 size-4" /> </Button>
-          </Link>
-        </div>
-      </div>
+    <Card className="my-24 mx-4 lg:!mx-auto flex bg-blue-950 md:w-[1024px] lg:w-[1224px] py-0 flex-col lg:flex-row items-center rounded-sm">
+      <CardHeader className="relative w-full h-[424px]">
+        <Image
+          className="mx-auto object-cover object-center"
+          src={post.image}
+          fill={true}
+          alt={post.title}
+        />
+      </CardHeader>
+      <CardContent className="flex flex-col py-4 px-4 items-start justify-start">
+        {post?.tags !== undefined ? (
+          <Badge className="mb-8 rounded-none text-xs font-bold uppercase tracking-widest  text-center">
+            {" "}
+            <Tag /> {post?.tags[0]}{" "}
+          </Badge>
+        ) : (
+          ""
+        )}
+        <CardTitle className="mb-8 text-4xl text-white font-semibold leading-12 md:text-4xl">
+          {post.title}{" "}
+        </CardTitle>
+        <CardDescription className="mb-8 text-lg text-white leading-8">
+          {post.description}{" "}
+        </CardDescription>
+        <Link
+          href={`/read/${post.slug}`}
+          className="hover:underline text-white flex flex-row items-center underline-offset-8 rounded-none"
+        >
+          Read Now <ArrowRight className="ml-2 size-4" />{" "}
+        </Link>
+      </CardContent>
     </Card>
   );
-};
+}
